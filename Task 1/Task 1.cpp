743d1ad5e5d4369cf7bb817d3c8ae3641c93a5b5
Kevin Gropasi <w2178400@westminster.ac.uk>
2025-11-14 20:47:27 +0000

    1st attempt at coding this project, still loads to do.

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-	cout << "Hello World" << endl;
+	int height; // delcaring loop variables
+	int HR1; int HR2; int HR3; int HR4; int HR5;
+	height = 0; //initialising loop variables;
+	cout << "Welcome to Meds & More Pharmacy, for all your pharmaceutical needs!" << endl;
+	while (height <= 199)
+	{
+		cout << "Enter your height: " << endl;
+		cin >> height;
+		height++;
+		if (height <=159)
+		{
+			cout << " Height Range 0-159: " << HR1 << endl;
+			HR1++;
+	}
+
 	return 0;
 }
