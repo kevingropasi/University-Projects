3598ab17eb4b4b3c9183ef5cff48b2896ef5b23c
Kevin Gropasi <w2178400@westminster.ac.uk>
2025-11-16 14:00:17 +0000

    Task 2 completed.

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 int main()
 { // declaring and initialising loop variables
-	double height = 0; double totalheights = 0; int HR1 = 0; int HR2 = 0; int HR3 = 0; int HR4 = 0; int HR5 = 0;
+	double height = 0; double totalheights = 0; double shortest = 200; double tallest = 0;
+	int HR1 = 0; int HR2 = 0; int HR3 = 0; int HR4 = 0; int HR5 = 0;
 	cout << "Welcome to Meds & More Pharmacy, for all your pharmaceutical needs!" << endl;
 	cout << "You can stop this program at any time by entering a number above the maximum height [199]" << endl;
-	while (height <= 199) // Beginning of Task 1, I'm using a 'while' loop structure to create an event-controlled loop
+	while (height < 199) // Beginning of Task 1, I'm using a 'while' loop structure to create an event-controlled loop
 	{
-		cout << "Enter the customer's height: "; cin >> height;totalheights++;//Entering values to then be filtered into each loop variable on line 5.
+		cout << "Enter the customer's height: "; cin >> height;totalheights++; //Entering values to then be filtered into each loop variable on line 5.
+		if (height < shortest) shortest = height;
+		if (height > tallest) tallest = height;
 		if (height <= 159) { //organising height ranges into if statements to filter integer values given by user on line 10.
 			HR1++;
 		}
@@ -36,5 +39,12 @@ int main()
 		cout << "Total number of customers in day whose heights were measured: " << totalheights << endl;
 		int	sum180 = totalheights - HR5 - HR4;
 		cout << "Total number of customers in a day below 180cm: " << sum180 << endl;
+		if (totalheights > 0) {
+			cout << "Shortest height measured: " << shortest << " cm" << endl;
+			cout << "Tallest height measured : " << tallest << " cm" << endl;
+		}
+		else {
+			cout << "No valid heights entered." << endl;
+		}
 		return 0;
 }
