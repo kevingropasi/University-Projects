17ea1a581eb84400bfd9fbe6d203045704275730
Kevin Gropasi <w2178400@westminster.ac.uk>
2025-11-19 16:37:18 +0000

    All tasks completed.

--- Task 1/Task 1.cpp
+++ Task 1/Task 1.cpp
@@ -8,25 +8,15 @@ int main()
 	while (height <= 199) // Beginning of Task 1, I'm using a 'while' loop structure to create an event-controlled loop
 	{
 		cout << "Enter the customer's height: "; cin >> height; //Entering values to then be filtered into each loop variable on line 5.
-		if (height <= 159) { //organising height ranges into if statements to filter integer values given by user on line 10.
-			HR1++;
-		} 
-		else if (height >= 160 and height <= 169) {
-			HR2++;
-		}
-		else if (height >= 170 and height <= 179) {
-			HR3++;
-		}
-		else if (height >= 180 and height <= 189) {
-			HR4++;
-		}
-		else if (height >= 190 and height <= 199) {
-			HR5++;
-		}
+		if (height <= 159) HR1++; //organising height ranges into else if statements to filter integer values given by user on line 10.
+		else if (height <= 169) HR2++;
+		else if (height <= 179) HR3++;
+		else if (height <= 189) HR4++;
+		else if (height <= 199) HR5++;
 	}
 	if (height >= 199) { // creating an output display for the horizontal histogram using for loops
-		cout << "      Histogram       " << endl;
-		cout << "Height Range   0-159: "; for (int i = 0; i < HR1; i++) cout << "+"; cout << endl;
+		cout << "       Histogram      " << endl;
+		cout << "Height Range   0-159  "; for (int i = 0; i < HR1; i++) cout << "+"; cout << endl;
 		cout << "Height Range 160-169: "; for (int i = 0; i < HR2; i++) cout << "+"; cout << endl;
 		cout << "Height Range 170-179: "; for (int i = 0; i < HR3; i++) cout << "+"; cout << endl;
 		cout << "Height Range 180-189: "; for (int i = 0; i < HR4; i++) cout << "+"; cout << endl;
--- Task 3/Task 3.cpp
+++ Task 3/Task 3.cpp
@@ -1,36 +1,44 @@
-#include <iostream> <string>
+#include <iostream> 
+#include <string>
 using namespace std;
 int main()
 { // declaring and initialising loop variables
-	double height = 0; int HR1 = 0; int HR2 = 0; int HR3 = 0; int HR4 = 0; int HR5 = 0;
+	int height = 0; int HR1 = 0; int HR2 = 0; int HR3 = 0; int HR4 = 0; int HR5 = 0;
 	cout << "Welcome to Meds & More Pharmacy, for all your pharmaceutical needs!" << endl;
 	cout << "You can stop this program at any time by entering a number above the maximum height [199]" << endl;
 	while (height <= 199) // Beginning of Task 1, I'm using a 'while' loop structure to create an event-controlled loop
 	{
 		cout << "Enter the customer's height: "; cin >> height; //Entering values to then be filtered into each loop variable on line 5.
-		if (height <= 159) HR1++; //organising height ranges into if statements to filter integer values given by user on line 10.
+		if (height <= 159) HR1++; //organising height ranges into else if statements to filter integer values given by user on line 10.
 		else if (height <= 169) HR2++;
 		else if (height <= 179) HR3++;
 		else if (height <= 189) HR4++;
 		else if (height <= 199) HR5++;
 	}
-	if (height >= 199) { // creating an output display for the vertical histogram using for loops
-		cout << "      Histogram       " << endl;
-		int maxCount = max({HR1, HR2, HR3, HR4, HR5});
-		for (int row = maxCount; row > 0; row--) {
-			if (HR1 >= row) cout << " + ";
+	if (height >= 199);
+	{ // creating an output display for the horizontal histogram using for loops
+		cout << "       Histogram      " << endl;
+		int max = HR1;
+		if (HR2 > max) max = HR2;
+		if (HR3 > max) max = HR3;
+		if (HR4 > max) max = HR4;
+		if (HR5 > max) max = HR5;
+		for (int row = max; row > 0; row--)
+		{
+			if (HR1 >= row) cout << "  +   ";
 			else cout << "   ";
-			if (HR2 >= row) cout << " + ";
+			if (HR2 >= row) cout << "    +   ";
 			else cout << "   ";
-			if (HR3 >= row) cout << " + ";
+			if (HR3 >= row) cout << "    +   ";
 			else cout << "   ";
-			if (HR4 >= row) cout << " + ";
+			if (HR4 >= row) cout << "    +   ";
 			else cout << "   ";
-			if (HR5 >= row) cout << " + ";
+			if (HR5 >= row) cout << "    +   ";
 			else cout << "   ";
 			cout << endl;
 		}
-			cout << "---------------------------------------------------" << endl;
-			cout << " 0-159    160-169    170-179    180-189    190-199 " << endl;
+		cout << "---------------------------------------" << endl;
+		cout << "|0-159|160-169|170-179|180-189|190-199|" << endl;
+	}
 	return 0;
-		}
+}
